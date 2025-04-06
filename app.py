a995d40d828b137a5fd8c9e149b53ca32d47bff1
CarolinaMercado184 <caro1992mercadi@gmail.com>
2025-04-06 14:36:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,9 @@ def init_db():
     ''')
     # Crear tabla de stock
     cursor.execute('''
+    @app.route('/')
+def home():
+    return render_template('index.html')
         CREATE TABLE IF NOT EXISTS stock (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             tipo TEXT,
